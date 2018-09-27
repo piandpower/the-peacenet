c2872eb0afe91cf8a2f24d31bc3977ffd255a8e3
The Fuzzy Riolu <31224503+TheFuzzyRiolu@users.noreply.github.com>
2018-09-27 09:16:44 -0400

    Seed generator - thanks Declan. :)

--- Source/ProjectOglowia/Private/UWorldGenerator.cpp
+++ Source/ProjectOglowia/Private/UWorldGenerator.cpp
@@ -9,9 +9,23 @@ int32 UWorldGenerator::GetSeedFromString(const FString& InSeedString)
 		return FCString::Atoi(*InSeedString);
 	}
 
+	if (InSeedString.IsEmpty())
+		return 8675309;
+
 	TArray<TCHAR> Chars = InSeedString.GetCharArray();
 
-	return 0;
+	// Below algorithm is written by Declan Hoare (NetwideRogue).
+
+	int result = 0;
+	int charCount = Chars.Num();
+
+	for (int i = 0; i < charCount; i++)
+	{
+		uint8 c = (uint8)Chars[charCount];
+		result ^= c << (((i + 1) & 3) << 3);
+	}
+
+	return result;
 }
 
 FRandomStream UWorldGenerator::GetRandomNumberGenerator(int32 InSeed)

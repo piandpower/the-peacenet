33de6879398e87b08f37d8c6fe6af6ff71d86d6d
Alkaline Thunder <theplexgate@gmail.com>
2018-11-25 09:50:24 -0500

    Add a class that handles a successful hack of a service.

---
+++ Source/ProjectOglowia/Private/UHackableHandler.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2018 The Peacenet & Alkaline Thunder.
+
+#include "UHackableHandler.h"
+
+bool UHackableHandler::CanHandle(USystemContext* InCaller)
+{
+	return false;
+}
+
+void UHackableHandler::Handle(USystemContext* InCaller, USystemContext* InTarget)
+{
+
+}
---
+++ Source/ProjectOglowia/Public/UHackableHandler.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2018 The Peacenet & Alkaline Thunder.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "USystemContext.h"
+#include "UHackableHandler.generated.h"
+
+/**
+ * A handler class that is used to decide what happens when you successfully gain access to a computer service.
+ */
+UCLASS(BlueprintType, Abstract, EditInlineNew)
+class PROJECTOGLOWIA_API UHackableHandler : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION()
+	virtual bool CanHandle(USystemContext* InCaller);
+
+	UFUNCTION()
+	virtual void Handle(USystemContext* InCaller, USystemContext* InTarget);
+};

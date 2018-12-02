533a8900a6d4d74f645040ce71633398daccc71f
Alkaline Thunder <theplexgate@gmail.com>
2018-12-02 12:38:46 -0500

    Add a GUI scale setting

--- Source/ProjectOglowia/Public/UPeacenetSettings.h
+++ Source/ProjectOglowia/Public/UPeacenetSettings.h
@@ -6,6 +6,14 @@
 #include "GameFramework/SaveGame.h"
 #include "UPeacenetSettings.generated.h"
 
+UENUM(BlueprintType)
+enum class EGUIScale : uint8
+{
+	Normal,
+	Medium,
+	Large
+};
+
 /**
  * A save game object that contains all of Peacenet's settings.
  */
@@ -15,4 +23,6 @@ class PROJECTOGLOWIA_API UPeacenetSettings : public USaveGame
 	GENERATED_BODY()
 
 public:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings|UI")
+	EGUIScale GUIScale = EGUIScale::Normal;
 };

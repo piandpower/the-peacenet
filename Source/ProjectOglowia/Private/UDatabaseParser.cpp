4222718017ea2830716c5c3f2a81ea64628e57b4
Alkaline Thunder <theplexgate@gmail.com>
2018-12-08 14:25:51 -0500

    Create a database reader function

---
+++ Source/ProjectOglowia/Private/UDatabaseParser.cpp
@@ -0,0 +1,77 @@
+// Copyright (c) 2018 The Peacenet & Alkaline Thunder.
+
+#include "UDatabaseParser.h"
+
+TArray<FDatabaseTable> UDatabaseParser::ParseTables(FString InTableString)
+{
+	FString CurrentTableName;
+	TArray<FString> ColumnNames;
+	TArray<FDatabaseRow> Rows;
+
+	check(!InTableString.IsEmpty());
+
+	TArray<FDatabaseTable> Tables;
+
+	TArray<FString> Lines;
+	InTableString.ParseIntoArray(Lines, TEXT("\n"), true);
+
+	for (auto Line : Lines)
+	{
+		if (Line.TrimStartAndEnd().IsEmpty())
+			continue;
+
+		if (Line.StartsWith("\t"))
+		{
+			check(!CurrentTableName.IsEmpty());
+			
+			if (!ColumnNames.Num())
+			{
+				Line.ParseIntoArray(ColumnNames, TEXT("\t"), true);
+				check(ColumnNames.Num());
+
+				TArray<FString> ColumnCheck;
+				for (auto Column : ColumnNames)
+				{
+					check(!ColumnCheck.Contains(Column));
+					ColumnCheck.Add(Column);
+				}
+			}
+			else
+			{
+				TArray<FString> Row;
+				Line.ParseIntoArray(Row, TEXT("\t"), true);
+				check(Row.Num() == ColumnNames.Num());
+				
+				TMap<FString, FString> ColumnMap;
+				
+				for (int i = 0; i < Row.Num(); i++)
+				{
+					ColumnMap.Add(ColumnNames[i], Row[i]);
+				}
+
+				FDatabaseRow NewRow;
+				NewRow.Columns = ColumnMap;
+				Rows.Add(NewRow);
+			}
+		}
+		else
+		{
+			if (!CurrentTableName.IsEmpty())
+			{
+				check(ColumnNames.Num());
+
+				FDatabaseTable NewTable;
+				NewTable.Name = CurrentTableName;
+				NewTable.Rows = Rows;
+
+				Tables.Add(NewTable);
+			}
+
+			CurrentTableName = Line.TrimStartAndEnd();
+			ColumnNames.Empty();
+			Rows.Empty();
+		}
+	}
+
+	return Tables;
+}
---
+++ Source/ProjectOglowia/Public/UDatabaseParser.h
@@ -0,0 +1,44 @@
+// Copyright (c) 2018 The Peacenet & Alkaline Thunder.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UDatabaseParser.generated.h"
+
+USTRUCT()
+struct PROJECTOGLOWIA_API FDatabaseRow
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY()
+	TMap<FString, FString> Columns;
+};
+
+USTRUCT()
+struct PROJECTOGLOWIA_API FDatabaseTable
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY()
+	FString Name;
+
+	UPROPERTY()
+	TArray<FDatabaseRow> Rows;
+};
+
+
+
+/**
+ * A class for parsing a Peacenet .db file.
+ */
+UCLASS(BlueprintType)
+class PROJECTOGLOWIA_API UDatabaseParser : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION()
+	static TArray<FDatabaseTable> ParseTables(FString InTableString);
+};

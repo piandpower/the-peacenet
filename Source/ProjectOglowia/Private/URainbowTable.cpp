f79faa76da0987ef7575b701f3e0a3acbd389325
Alkaline Thunder <theplexgate@gmail.com>
2018-12-12 21:27:20 -0500

    Add a Rainbow Table object that encapsulates all rainbow table operations.

---
+++ Source/ProjectOglowia/Private/URainbowTable.cpp
@@ -0,0 +1,49 @@
+// Copyright (c) 2018 The Peacenet & Alkaline Thunder.
+
+#include "URainbowTable.h"
+#include "USystemContext.h"
+#include "UPeacegateFileSystem.h"
+
+void URainbowTable::Setup(USystemContext* InSystem, FString InPath)
+{
+	// Some safeguards against drunk developers like Alkaline
+	check(InSystem);
+	check(!InPath.TrimStartAndEnd().IsEmpty());
+	check(InPath.StartsWith("/"));
+	check(!InPath.EndsWith("/"));
+
+	// We get owned by the calling system context.
+	this->SystemContext = InSystem;
+
+	// And, we get a root filesystem.
+	this->Filesystem = this->SystemContext->GetFilesystem(0);
+
+	// More debug asserts to correct the mistakes of drunk devs.
+	check(this->SystemContext);
+	check(this->Filesystem);
+
+	// Now set the path for the rainbow table.
+	this->RainbowTablePath = InPath;
+
+	// Check to make sure the rainbow table IS NOT A FUCKING FOLDER.
+	check(!this->Filesystem->DirectoryExists(this->RainbowTablePath));
+
+	// Now we can check to see if we have a rainbow table to read.
+	if (this->Filesystem->FileExists(this->RainbowTablePath))
+	{
+		// The rainbow table is just a database, like any .db file. So we can use the functions in UDatabase and UDatabaseParser to help us.
+		
+		// This value is populated by the filesystem engine for certain operations and allows us to check what really happened with read/write ops. If something went wrong, it'll tell you, instead of asserting... since the player can easily fuck things up.
+		EFilesystemStatusCode DatabaseLoadStatus;
+		
+		// TODO: Pre-emptive recovery/recreation of the database if this fails. We want to be silent about it, we don't want to assert. The player can break this.
+		check(UDatabase::ReadFromFile(this->Filesystem, this->RainbowTablePath, DatabaseLoadStatus, this->RainbowTable));
+
+		// TODO: Updating of the database schema, checking data integrity, etc.
+	}
+	else
+	{
+		// TODO: Creation of a new (empty) rainbow table database.
+		check(false);
+	}
+}
---
+++ Source/ProjectOglowia/Public/URainbowTable.h
@@ -0,0 +1,48 @@
+// Copyright (c) 2018 The Peacenet & Alkaline Thunder.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UDatabase.h"
+#include "UDatabaseParser.h"
+#include "URainbowTable.generated.h"
+
+class USystemContext;
+class UPeacegateFileSystem;
+
+/**
+ * Encapsulates functionality for reading from/writing to a Peacenet Rainbow Table file.
+ */
+UCLASS(BlueprintType)
+class PROJECTOGLOWIA_API URainbowTable : public UObject
+{
+	GENERATED_BODY()
+
+	// If there's anything I've learned from GAME102 when it comes to OOP, it's that:
+	// 1. It really stands for "Overly Obfuscated Projects."
+	// 2. Encapsulation.
+	//
+	// - Alkaline Thunder
+
+protected:
+	// The system that owns this rainbow table manager.
+	UPROPERTY()
+	USystemContext* SystemContext;
+
+	// The filesystem, preferrably with root permissions, that we'll use to read the rainbow table.
+	UPROPERTY()
+	UPeacegateFileSystem* Filesystem;
+
+	// The path to the rainbow table.
+	UPROPERTY()
+	FString RainbowTablePath;
+
+	// This is the underlying database object that holds the actual rainbow table data.
+	UPROPERTY()
+	UDatabase* RainbowTable;
+
+public:
+	// Set up this rainbow table manager with the given system context and file path.
+	UFUNCTION()
+	void Setup(USystemContext* InSystem, FString InPath);
+};

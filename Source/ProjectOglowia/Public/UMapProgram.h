6434f93f450e01e679e4816c0b787251d8070d4b
Alkaline Thunder <theplexgate@gmail.com>
2019-01-23 10:55:43 -0500

    Add a map window.

---
+++ Source/ProjectOglowia/Public/UMapProgram.h
@@ -0,0 +1,15 @@
+// Copyright (c) 2019 Alkaline Thunder & Bit Phoenix Software
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UProgram.h"
+#include "UUserContext.h"
+#include "FPeacenetIdentity.h"
+#include "UMapProgram.generated.h"
+
+UCLASS(Blueprintable, BlueprintType, Abstract)
+class PROJECTOGLOWIA_API UMapProgram : public UProgram
+{
+    GENERATED_BODY()
+};
